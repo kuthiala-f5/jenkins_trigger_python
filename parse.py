5485cd2773b6bf3495b75c3c0f486ee12a360de2
Aniruddh Kuthiala <a.kuthiala@f5.com>
2018-06-16 13:53:20 -0700

    Only parse for the non beta versions.

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@ def main():
     url = 'https://github.com/f5devcentral/f5-sphinx-theme/releases.atom'
     try:
         file_contents = urllib.urlopen(url).read()
-        pattern = re.compile("<id>tag:github.com.*?(v\d+\.\d+\.\d+.*)</id>\n.*?<updated>(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)")
+        pattern = re.compile("<id>tag:github.com.*?(v\d+\.\d+\.\d+)</id>\n.*?<updated>(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)")
         releases = re.findall(pattern, file_contents)
         for i in releases:
             utc_time = time.strptime(i[1], "%Y-%m-%dT%H:%M:%SZ")
