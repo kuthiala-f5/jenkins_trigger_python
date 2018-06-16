22fe4cb591d076ab6a26ed11e01c039b02a9b0e6
Aniruddh Kuthiala <a.kuthiala@f5.com>
2018-06-15 23:20:35 -0700

    get release time of f5-sphinx-theme

---
+++ parse.py
@@ -0,0 +1,21 @@
+import re
+import time
+from calendar import timegm
+import urllib
+
+def main():
+    url = 'https://github.com/f5devcentral/f5-sphinx-theme/releases.atom'
+    file_contents = urllib.urlopen(url).read()
+    pattern = re.compile("<id>tag:github.com.*?(v\d+\.\d+\.\d+.*)</id>\n.*?<updated>(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)")
+    releases = re.findall(pattern, file_contents)
+    for i in releases:
+        utc_time = time.strptime(i[1], "%Y-%m-%dT%H:%M:%SZ")
+        release_time_since_epoch = timegm(utc_time)
+        if int(time.time()-release_time_since_epoch) <= 300:
+            print 'build'
+            break
+    else:
+        print 'no_build'
+
+if __name__ == '__main__':
+    main()
